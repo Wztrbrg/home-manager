206543aea00acd8e1b72488a168f25ba2ae72a28
Jonathan <2jonand@gmail.com>
2024-02-27 13:52:09 +0100

    added sass and some basic structure to build from

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import MainWindow from "./components/MainWindow";
+import "./assets/style/global/app.scss";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <MainWindow />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/MainWindow.jsx
@@ -0,0 +1,13 @@
+import "../assets/style/mainwindow.scss"
+import SideBar from "./SideBar";
+
+function MainWindow () {
+
+  return (
+    <div className="main-window">
+      <SideBar />
+    </div>
+  )
+}
+
+export default MainWindow;
